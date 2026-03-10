2ef831302799ee07ee4320c42927768b462d59fd
hrspnd <8alexapineda8@gmail.com>
2026-03-10 21:25:24 +0800

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,14 @@
+"""
+Training Script
+
+This script will train the intent classification models for the
+delivery customer support assistant.
+
+Planned functionality:
+- Load processed dataset
+- Train baseline models (Naive Bayes, SVM)
+- Train deep learning model (BERT)
+- Save trained models and training logs
+
+Note: Implementation will be added in later milestones.
+"""
