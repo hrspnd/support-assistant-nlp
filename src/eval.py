af850177e98fbb57a07324c193b471a1041f2203
hrspnd <8alexapineda8@gmail.com>
2026-03-10 21:25:50 +0800

    Create eval.py

---
+++ src/eval.py
@@ -0,0 +1,15 @@
+"""
+Evaluation Script
+
+This module will evaluate the performance of the trained models.
+
+Planned evaluation metrics:
+- Accuracy
+- Precision
+- Recall
+- F1 Score
+
+Evaluation results will be logged and stored for analysis.
+
+Note: Implementation will be added in later milestones.
+"""
